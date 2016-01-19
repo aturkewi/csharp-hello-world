11a89b71116372b59a9880dfd0a927bd85b28653
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-01-19 09:12:32 -0500

    Initial commit

---
+++ CSharpFromScratchHelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpFromScratchHelloWorld
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# C Sharp From Scratch - Hello World
+
+This simple app will outout hello world to your console. Learning CSharp with Pluralsight CSharp from scratch videos.
